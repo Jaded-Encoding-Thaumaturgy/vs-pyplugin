fa92db489a573e6fbc0537a09e140ada453f750f
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-13 02:41:58 +0200

    Rename get_frames, add function

--- vspyplugin/helpers.py
+++ vspyplugin/helpers.py
@@ -16,7 +16,8 @@ __all__ = [
     'FrameRequest', 'SingleFrameRequest', 'GatherRequests',
     'Atom',
 
-    'get_frame', 'get_frames', 'gather',
+    'get_frame', 'get_frames', 'get_frames_shifted',
+    'gather',
 
     'frame_eval', 'frame_eval_async',
 
@@ -121,7 +122,11 @@ async def get_frame(clip: vs.VideoNode, frame_no: int) -> vs.VideoFrame:
     return await SingleFrameRequest(clip, frame_no)
 
 
-async def get_frames(
+async def get_frames(*clips: vs.VideoNode, frame_no: int) -> tuple[vs.VideoFrame, ...]:
+    return await gather(*(get_frame(clip, frame_no) for clip in clips))
+
+
+async def get_frames_shifted(
     clip: vs.VideoNode, frame_no: int, shifts: int | tuple[int, int] | Iterable[int] = (-1, 1)
 ) -> tuple[vs.VideoFrame, ...]:
     if isinstance(shifts, int):
