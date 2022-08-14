94fa7c0155d818ad940733c807144efe2e19db68
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-15 00:44:56 +0200

    Implement Cython backend

--- vspyplugin/cython.py
+++ vspyplugin/cython.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Sequence
+
+import vapoursynth as vs
+
 from .backends import PyBackend
 from .base import FD_T, PyPlugin, PyPluginUnavailableBackend
 
@@ -9,9 +14,162 @@ __all__ = [
 
 this_backend = PyBackend.CYTHON
 
+uniqey = 'cython'
+
+
 try:
+    from Cython.Build import cythonize  # noqa: F401
+
+    class CythonKernelFunction:
+        def __init__(self, function: Any) -> None:
+            self.function = function
+
+        if TYPE_CHECKING:
+            def __call__(
+                self, src: memoryview | list[memoryview], dst: memoryview | list[memoryview], *args: Any
+            ) -> Any:
+                ...
+        else:
+            def __call__(self, *args: Any) -> Any:
+                return self.function(*args)
+
+    class CythonKernelFunctions:
+        def __init__(self, **kwargs: CythonKernelFunction) -> None:
+            for key, func in kwargs.items():
+                setattr(self, key, func)
+
+        if TYPE_CHECKING:
+            def __getattribute__(self, __name: str) -> CythonKernelFunction:
+                ...
+
     class PyPluginCython(PyPlugin[FD_T]):
         backend = this_backend
+
+        cython_kernel: tuple[str | Path, str | Sequence[str]]
+
+        kernel_kwargs: dict[str, Any]
+
+        kernel: CythonKernelFunctions
+
+        def __init__(self, ref_clip: vs.VideoNode, clips: list[vs.VideoNode] | None = None, **kwargs: Any) -> None:
+            import random
+            import string
+            import subprocess
+            from hashlib import md5
+            from importlib.util import module_from_spec, spec_from_file_location
+            from os import listdir, makedirs, remove
+            from shutil import copyfile, move
+
+            super().__init__(ref_clip, clips, **kwargs)
+
+            assert self.ref_clip.format
+
+            class_name = self.__class__.__name__
+
+            if not hasattr(self, 'cython_kernel'):
+                raise RuntimeError(f'{class_name}: You\'re missing cython_kernel!')
+
+            cython_path, cython_functions = self.cython_kernel
+            if isinstance(cython_functions, str):
+                cython_functions = [cython_functions]
+
+            if not isinstance(cython_path, Path):
+                cython_path = Path(cython_path)
+
+            cython_path = cython_path.absolute().resolve()
+            if not cython_path.exists():
+                raise RuntimeError(f'{class_name}: Cython code not found!')
+
+            if not cython_path.is_file():
+                raise NotImplementedError(f'{class_name}: Directories not yet supported!')
+
+            if cython_path.suffix != '.pyx':
+                raise RuntimeError(f'{class_name}: Cython code must be a .pyx file!')
+
+            cython_build_dir = cython_path.parent / '.vspyplugin'
+            curr_md5 = str(md5(cython_path.read_bytes()).digest())
+
+            if not cython_build_dir.exists():
+                makedirs(cython_build_dir, 0o777, True)
+
+            module_path: Path | None = None
+            curr_files = listdir(cython_build_dir)
+
+            md5filename = f'{uniqey}_{cython_path.stem}.md5'
+            old_builds = list[str]()
+
+            for file in curr_files:
+                if file == md5filename:
+                    md5file = cython_build_dir / file
+
+                    module_hash, module_md5 = md5file.read_text().splitlines()
+
+                    module_folder = cython_build_dir / module_hash
+
+                    if module_folder.exists():
+                        if curr_md5 == module_md5:
+                            module_path = module_folder
+                            break
+
+                        old_builds.append(module_hash)
+
+            if module_path is None:
+                cython_new_path = cython_build_dir / cython_path.name
+
+                copyfile(cython_path, cython_new_path)
+
+                subprocess.run([
+                    'cythonize', '--3str', '-i', '-q', '--lenient', '-k', cython_path.name
+                ], cwd=cython_new_path.parent)
+
+                new_files = {*listdir(cython_build_dir)} - {*curr_files}
+
+                rand_str = ''.join(random.choice(string.ascii_lowercase) for _ in range(12))
+                module_path = cython_build_dir / f'{uniqey}_{rand_str}'
+
+                makedirs(module_path)
+
+                for file in new_files:
+                    if file.endswith('.pyd'):
+                        curr_file, new_file = cython_build_dir / file, module_path / file
+                        move(curr_file, new_file)
+                        md5file = cython_build_dir / md5filename
+                        md5file.open('w').write(f'{uniqey}_{rand_str}\n{curr_md5}')
+                    elif not file.endswith('.md5'):
+                        remove(cython_build_dir / file)
+
+            if module_path is None:
+                raise RuntimeError(f'{class_name}: There was an error compiling the cython module!')
+
+            for old_build in old_builds:
+                for file in listdir(cython_build_dir / old_build):
+                    try:
+                        remove(cython_build_dir / old_build / file)
+                    except BaseException:
+                        ...
+
+            pyd_file = next((file for file in listdir(module_path)), None)
+
+            if pyd_file is None:
+                raise RuntimeError(f'{class_name}: There was an error locating the cython module!')
+
+            spec = spec_from_file_location(cython_path.stem, module_path / pyd_file)
+
+            if spec is None:
+                raise RuntimeError(f'{class_name}: There was an error loading the cython module!')
+
+            module = module_from_spec(spec)
+
+            if spec.loader is None:
+                raise RuntimeError(f'{class_name}: The cython module is missing the package loader!')
+
+            spec.loader.exec_module(module)
+
+            self.kernel = CythonKernelFunctions(**{
+                name: CythonKernelFunction(
+                    object.__getattribute__(module, name)
+                ) for name in cython_functions
+            })
 
     this_backend.set_available(True)
 except BaseException as e:
