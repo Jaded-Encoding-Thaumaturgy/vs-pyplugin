e5b124920f875fbc2f62c0646ec6bc48dd6011fc
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-15 01:26:32 +0200

    Add Sima Filter Cython Example, from muvsfunc

---
+++ Examples/Cython/Sigma Filter/sigma_filter.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from ctypes import c_void_p
+
+from dataclasses import dataclass
+
+import vapoursynth as vs
+from stgfunc import set_output, source
+from vspyplugin import PyPluginCython
+from vspyplugin.base import PyPluginOptions
+
+core = vs.core
+
+
+@dataclass
+class SigmaFilterData:
+    radius: int
+    thr: float
+
+
+class SigmaFilter(PyPluginCython[SigmaFilterData]):
+    options = PyPluginOptions(float_processing=True)
+    cython_kernel = './sigma_filter.pyx', 'sigma_filter'
+    input_per_plane = True
+    output_per_plane = True
+
+    def process(self, f: vs.VideoFrame, src: c_void_p, dst: c_void_p, plane: int | None, n: int) -> None:
+        self.kernel.sigma_filter(src, dst, self.fd.radius, self.fd.thr)
+
+
+def sigma_filter(clip: vs.VideoNode, radius: int = 3, thr: float = 0.01) -> vs.VideoNode:
+    return SigmaFilter(clip, radius=radius, thr=thr).invoke()
+
+
+# Taken from
+# https://github.com/WolframRhodium/muvsfunc/blob/master/Collections/examples/SigmaFilter_cython/sigma_filter.pyx
+
+src = source(r"E:\Desktop\Encoding Sources\[BDMV] Takagi-San 3\TAKAGISAN3_1\BDMV\STREAM\00003.m2ts", 8, matrix_prop=1)
+src = src.std.ShufflePlanes(0, vs.GRAY)
+# src = src.resize.Bicubic(format=vs.YUV444P8)
+
+set_output(src)
+set_output(sigma_filter(src))
---
+++ Examples/Cython/Sigma Filter/sigma_filter.pyx
@@ -0,0 +1,38 @@
+# cython: boundscheck=False, initializedcheck=False, language_level=3, nonecheck=False, overflowcheck=False, wraparound=False
+
+cimport cython
+from cython cimport view
+from libc.math cimport fabs
+from cython.parallel import prange
+
+
+cdef int clamp(const int val, const int low, const int high) nogil:
+    return min(max(val, low), high)
+
+
+cpdef void sigma_filter(
+    const float [:, ::view.contiguous] src, float [:, ::view.contiguous] dst, 
+    const int radius, const float threshold
+) nogil:
+    cdef int height = src.shape[0]
+    cdef int width = src.shape[1]
+
+    cdef float center, val, acc
+    cdef int count, x, y, i, j
+
+    for y in prange(height):
+        for x in prange(width):
+            center = src[y, x]
+
+            acc = 0.0
+            count = 0
+
+            for j in prange(clamp(y - radius, 0, height - 1), clamp(y + radius, 0, height - 1) + 1):
+                for i in prange(clamp(x - radius, 0, width - 1), clamp(x + radius, 0, width - 1) + 1):
+                    val = src[j, i]
+
+                    if fabs(center - val) < threshold:
+                        acc += val
+                        count += 1
+            
+            dst[y, x] = acc / count
