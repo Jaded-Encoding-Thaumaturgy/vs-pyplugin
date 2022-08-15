d9d2a22a74dc587414366b30dfda98c784f3b4a7
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-15 17:14:00 +0200

    Update Bilateral Example, add decorator example

--- Examples/Cuda/Bilateral/bilateral_decorator.py
+++ Examples/Cuda/Bilateral/bilateral_decorator.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from functools import lru_cache
+from math import e, log2
 from typing import Any
 
 import vapoursynth as vs
@@ -12,49 +13,32 @@ core = vs.core
 
 
 class BilateralFilter(PyPluginCuda[None]):
-    cuda_kernel = 'bilateral'
-
-    input_per_plane = True
-    output_per_plane = True
-
-    kernel_size = 16
-    use_shared_memory = True
-
-    def process(self, f: vs.VideoFrame, src: NDArray[Any], dst: NDArray[Any], plane: int | None, n: int) -> None:
-        self.kernel.bilateral[plane](src, dst)
+    radius: int
 
     @lru_cache
     def calc_shared_mem(self, plane: int, blk_size_w: int, blk_size_h: int, dtype_size: int) -> int:
-        return (2 * self.bil_radius + blk_size_w) * (2 * self.bil_radius + blk_size_h) * dtype_size
-
-    def __init__(self, clip: vs.VideoNode, sigmaS: float, sigmaR: float, radius: int | None) -> None:
-        from math import e, log2
-
-        assert clip.format
-
-        sigmaS_scaled, sigmaR_scaled = [(-0.5 / (val * val)) * log2(e) for val in (sigmaS, sigmaR)]
-
-        if radius is None:
-            radius = max(1, round(sigmaS * 3))
-
-        self.bil_radius = radius
-
-        return super().__init__(
-            clip, kernel_kwargs=dict(
-                sigmaS=sigmaS_scaled, sigmaR=sigmaR_scaled,
-                radius=self.bil_radius
-            )
-        )
+        return (2 * self.radius + blk_size_w) * (2 * self.radius + blk_size_h) * dtype_size
 
 
 def bilateral(
     clip: vs.VideoNode, sigmaS: float = 3.0, sigmaR: float = 0.02, radius: int | None = None
 ) -> vs.VideoNode:
-    return BilateralFilter(clip, sigmaS, sigmaR, radius).invoke()
+    sigmaS_scaled, sigmaR_scaled = [(-0.5 / (val * val)) * log2(e) for val in (sigmaS, sigmaR)]
 
+    if radius is None:
+        radius = max(1, round(sigmaS * 3))
+
+    kernel_kwargs = dict(sigmaS=sigmaS_scaled, sigmaR=sigmaR_scaled, radius=radius)
+
+    @BilateralFilter(clip, None, 'bilateral', 16, True, radius=radius, kernel_kwargs=kernel_kwargs)
+    def output(self: BilateralFilter, src: NDArray[Any], dst: NDArray[Any], plane: int) -> None:
+        self.kernel.bilateral[plane](src, dst)
+
+    return output
 
 # Test - Compare with the original c++ plugin this is based off
 # From my benchmarks, it's 2x faster with real numbers, just 6% in the vacuum (BlankClip, with zeroes)
+
 
 src = source(r"E:\Desktop\Encoding Sources\[BDMV] Takagi-San 3\TAKAGISAN3_1\BDMV\STREAM\00003.m2ts", 8, matrix_prop=1)
 src = src.std.ShufflePlanes(0, vs.GRAY)
