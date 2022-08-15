3c0958ad119ccfd4f5e8def8e0fe4268934b78fb
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-15 15:34:00 +0200

    Add copy_signature type

--- vspyplugin/types.py
+++ vspyplugin/types.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Iterable, Protocol, TypeVar
+from typing import Any, Callable, Generic, Iterable, Protocol, TypeVar
 
 _KT = TypeVar('_KT')
 _VT_co = TypeVar('_VT_co', covariant=True)
@@ -11,4 +11,15 @@ class SupportsKeysAndGetItem(Protocol[_KT, _VT_co]):
         ...
 
     def __getitem__(self, __k: _KT) -> _VT_co:
+        ...
+
+
+F = TypeVar('F', bound=Callable[..., Any])
+
+
+class copy_signature(Generic[F]):
+    def __init__(self, target: F) -> None:
+        ...
+
+    def __call__(self, wrapped: Callable[..., Any]) -> F:
         ...
