b1e9ec0681e3503c69bcf691579bf32f24f91de9
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-23 16:29:15 +0200

    Add abstracts submodule

---
+++ vspyplugin/abstracts.py
@@ -0,0 +1,143 @@
+from __future__ import annotations
+
+from enum import IntEnum
+from typing import Any, Callable, Generic, Literal, Mapping, TypeAlias, TypeVar, Union, overload
+
+import vapoursynth as vs
+
+__all__ = [
+    'ProcessMode',
+    'PyPluginBackendBase'
+]
+
+F = TypeVar('F')
+FD_T = TypeVar('FD_T')
+DT_T = TypeVar('DT_T')
+
+PassthroughC = Callable[[F], F]
+
+
+class ProcessModeBase:
+    Any_T = Callable[[Any, DT_T | list[DT_T], DT_T | list[DT_T], vs.VideoFrame, int | None, int], None]
+    SingleSrcIPP_T = Callable[[Any, DT_T, DT_T, vs.VideoFrame, int, int], None]
+    MultiSrcIPP_T = Callable[[Any, list[DT_T], DT_T, vs.VideoFrame, int, int], None]
+    SingleSrcIPF_T = Callable[[Any, DT_T, list[DT_T], vs.VideoFrame, int], None]
+    MultiSrcIPF_T = Callable[[Any, list[DT_T], list[DT_T], vs.VideoFrame, int], None]
+
+
+class ProcessMode(ProcessModeBase, IntEnum):
+    Any = -1
+    SingleSrcIPP = 0
+    MultiSrcIPP = 1
+    SingleSrcIPF = 2
+    MultiSrcIPF = 3
+
+
+ALL_PMODES = Union[
+    ProcessMode.Any_T[DT_T],
+    ProcessMode.SingleSrcIPP_T[DT_T], ProcessMode.MultiSrcIPP_T[DT_T],
+    ProcessMode.SingleSrcIPF_T[DT_T], ProcessMode.MultiSrcIPF_T[DT_T]
+]
+
+
+class PyPluginBackendOverloadedDict(dict[str, Any]):
+    def __setitem__(self, key: str, value: Any) -> None:
+        overloaded = getattr(value, '__pybackend_overload__', False)
+
+        if overloaded:
+            mode: ProcessMode = value.__pybackend_mode__
+
+            if mode is ProcessMode.Any:
+                for pmode in ProcessMode:
+                    if pmode is ProcessMode.Any:
+                        continue
+
+                    mode_key = self.get_key(pmode)
+
+                    if self.get(mode_key, None) is None:
+                        super().__setitem__(mode_key, value)
+
+                return
+
+            key = self.get_key(mode)
+
+        super().__setitem__(key, value)
+
+    @staticmethod
+    def get_key(mode: ProcessMode) -> str:
+        return f'process_{mode.name}'
+
+
+class PyPluginBackendMeta(Generic[DT_T], type):
+    process_SingleSrcIPP: ProcessMode.SingleSrcIPP_T[DT_T] | None
+    process_MultiSrcIPP: ProcessMode.MultiSrcIPP_T[DT_T] | None
+    process_SingleSrcIPF: ProcessMode.SingleSrcIPF_T[DT_T] | None
+    process_MultiSrcIPF: ProcessMode.MultiSrcIPF_T[DT_T] | None
+
+    @classmethod
+    def __prepare__(metacls, name: str, bases: tuple[type, ...], /, **kwargs: Any) -> Mapping[str, object]:
+        return PyPluginBackendOverloadedDict(
+            process_SingleSrcIPP=None, process_MultiSrcIPP=None,
+            process_SingleSrcIPF=None, process_MultiSrcIPF=None,
+            **kwargs
+        )
+
+
+class PyPluginBackendBase(Generic[DT_T], metaclass=PyPluginBackendMeta[DT_T]):  # type: ignore
+    DT: TypeAlias = DT_T  # type: ignore
+    DTL: TypeAlias = list[DT_T]  # type: ignore
+    DTA: TypeAlias = DT_T | list[DT_T]  # type: ignore
+
+    @overload
+    @staticmethod
+    def process(mode: Literal[ProcessMode.Any], /) -> PassthroughC[ProcessMode.Any_T[DT_T]]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(mode: Literal[ProcessMode.SingleSrcIPP], /) -> PassthroughC[ProcessMode.SingleSrcIPP_T[DT_T]]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(mode: Literal[ProcessMode.MultiSrcIPP], /) -> PassthroughC[ProcessMode.MultiSrcIPP_T[DT_T]]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(mode: Literal[ProcessMode.SingleSrcIPF], /) -> PassthroughC[ProcessMode.SingleSrcIPF_T[DT_T]]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(mode: Literal[ProcessMode.MultiSrcIPF], /) -> PassthroughC[ProcessMode.MultiSrcIPF_T[DT_T]]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(func: ProcessMode.Any_T[DT_T], /) -> ProcessMode.Any_T[DT_T]:
+        ...
+
+    @overload
+    @staticmethod
+    def process(func: None, /) -> PassthroughC[PassthroughC[ProcessMode.Any_T[DT_T]]]:
+        ...
+
+    @staticmethod  # type: ignore
+    def process(mode_or_func: ProcessMode | ALL_PMODES[DT_T] | None = None, /) -> (
+        PassthroughC[ALL_PMODES[DT_T]] | ProcessMode.Any_T[DT_T]
+    ):
+        if mode_or_func is None:
+            return PyPluginBackendBase.process  # type: ignore
+        elif not isinstance(mode_or_func, ProcessMode):
+            return PyPluginBackendBase.process(ProcessMode.Any)(mode_or_func)  # type: ignore
+
+        def _wrapper(func: Callable[..., None]) -> Callable[..., None]:
+            func.__dict__.update(
+                __pybackend_overload__=True,
+                __pybackend_mode__=mode_or_func
+            )
+
+            return func
+
+        return _wrapper
