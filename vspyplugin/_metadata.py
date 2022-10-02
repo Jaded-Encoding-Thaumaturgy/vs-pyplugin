3d18e6a7ae620b33ef55d517f593af87744795fd
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-10-02 12:32:24 +0200

    Bump major version

--- vspyplugin/_metadata.py
+++ vspyplugin/_metadata.py
@@ -1,6 +1,6 @@
 """Package for simplifying writing VapourSynth "plugins" in python."""
 
-__version__ = '0.0.0'
+__version__ = '1.0.0'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
