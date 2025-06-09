3182a1b63b78e8a4cb33f4864c1175e081268950
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-06-09 19:09:34 +0000

    Bump version to v1.4.5

--- vspyplugin/_metadata.py
+++ vspyplugin/_metadata.py
@@ -1,6 +1,6 @@
 """Package for simplifying writing VapourSynth "plugins" in python."""
 
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 
 __author_name__, __author_email__ = 'Setsugen no ao', 'setsugen@setsugen.dev'
 __maintainer_name__, __maintainer_email__ = __author_name__, __author_email__
